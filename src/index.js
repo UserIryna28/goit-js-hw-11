a9fd35575f06805c1c2b439e49228f2a5597c5fe
UserIryna28 <Ltdcom@ukr.net>
2023-05-30 09:32:30 +0300

    Update files h-w 11

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
-
+// import { fetchImages } from './js/fetchImages.js'
 // Описаний в документації
 import SimpleLightbox from "simplelightbox";
 // Додатковий імпорт стилів
@@ -10,67 +10,99 @@ const form = document.querySelector("#search-form")
 const buttonLoadMore = document.querySelector(".load-more")
 const gallery = document.querySelector(".gallery")
 
+
 let query = ""
 let page = 1;
 const perPage = 40;
+let simpleLightBox
 
 
-fetchImages(query, page, perPage) {
-    try {
-        const response = await fetch(url);
-        if (response.ok) {
-            const data = await response.json();
-            return data.hits;
-        } else {
-            throw new Error('Request failed');
-        }
-    } catch (error) {
-        console.log(error);
-        return [];
-    }
+
+
+
+
+BASE_URL = "https://pixabay.com/api/"
+TOKEN = "36715459-28e84205f82fac02ca5e4373e"
+
+async function fetchImages(page = 1) {   
+return fetch(
+    `${BASE_URL}?key=${TOKEN}&page=${page}`,
+).then(response => {
+    if (!response.ok) {
+            throw new Error(response.statusText)
+       }
+        return response.json()   
+       })  
 }
+   
+  fetchImages()
+      .then(data => {
+ gallery.insertAdjacentHTML("beforeend", createCard(data.hits))
+      })
 
-
-
+    
+function createCard(arr) {
+  return arr.map(({ id, largeImageURL, webformatURL, tags, likes, views, comments, downloads }) =>
+       `
+        <a class="gallery__link" href="${largeImageURL}">
+          <div class="gallery-item" id="${id}">
+            <img class="gallery-item__img" src="${webformatURL}" alt="${tags}" loading="lazy" />
+            <div class="info">
+              <p class="info-item"><b>Likes</b>${likes}</p>
+              <p class="info-item"><b>Views</b>${views}</p>
+              <p class="info-item"><b>Comments</b>${comments}</p>
+              <p class="info-item"><b>Downloads</b>${downloads}</p>
+            </div>
+          </div>
+        </a>
+      `
+    )
+    .join('')
+}
+ 
 form.addEventListener("click", onForm)
 
 buttonLoadMore.addEventListener("submit",onButtonLoadMore)
-// створюємо картку зображення
-function createCard(images) {
-    const markup = images
-        .map(image => {
-            const { id, largeImageURL, webformatURL, tags, likes, views, comments, downloads } = image
-            return `
-         <a class="gallery__link" href="${largeImageURL}">
-          <div class="gallery-item" id="${id}"></div>
-    <img src="${webformatURL}" alt="${tags}" loading="lazy" />
-      <div class="info">
-        <p class="info-item">
-          <b>Likes</b>${likes}</p>
-        <p class="info-item">
-          <b>Views</b>${views}</p>
-        <p class="info-item">
-          <b>Comments</b>${comments}</p>
-        <p class="info-item">
-          <b>Downloads</b>${downloads}</p>
-            </div>
-        </div>
-         </a>  `
-        }).join("")
-    gallery.insertAdjacentHTML('beforeend', markup)
-}
-    
-    
+
+
+
 function onForm(evt) {
     evt.preventDefault
-    clearGallery()
-
     if (query.trim() === "") {
-    Notify.failure("Sorry, there are no images matching your search query. Please try again.")
-}
-
+        Notify.failure("Please enter a search query")
+        return
     }
+}  
 
- function clearGallery() {
-    gallery.innerHTML = '';
+async function onButtonLoadMore() {
+    currentPage += 1;
+    const image = await fetchImages(query, page);
+    const totalPages = Math.ceil(data.totalHits / perPage)
+    if (page > totalPages) {
+        createCard(image);
+    } else {
+        buttonLoadMore.style.display = 'none';
+        Notify.info("We're sorry, but you've reached the end of search results.");
+    }
 }
+// function onButtonLoadMore() {
+//    page += 1
+//    simpleLightBox.destroy()
+
+//    fetchImages(query, page, perPage)
+//      .then(({ data }) => {
+//        createCard(data.hits)
+//        simpleLightBox = new SimpleLightbox('.gallery a').refresh()
+
+//        const totalPages = Math.ceil(data.totalHits / perPage)
+
+//        if (page > totalPages) {
+//          buttonLoadMore.classList.add('is-hidden')
+//          Notify.failure("We're sorry, but you've reached the end of search results.")
+//        }
+//      })
+//      .catch(error => console.log(error))
+//  }
+//  function clearGallery() {
+//     gallery.innerHTML = '';
+// }
--- src/js/fetchImages.js
+++ src/js/fetchImages.js
@@ -1,9 +1,23 @@
-import axios from "axios";
- export {fetchImages}
+// import axios from "axios";
+// export { fetchImages }
 
-BASE_URL = "https://pixabay.com/api/"
-// //  const ENDPOINT = "/image_type"
-TOKEN = "36715459-28e84205f82fac02ca5e4373e"
+// BASE_URL = "https://pixabay.com/api/"
+// TOKEN = "36715459-28e84205f82fac02ca5e4373e"
+
+// async function fetchImages(page = 1) {   
+// return fetch(
+//     `${BASE_URL}?key=${TOKEN}&page=${page}`,
+// ).then(response => {
+//        if (!response.ok) {
+//            throw new Error(response.statusText)
+//       }
+//        return response.json()   
+//       })  
+//   }
+
+// BASE_URL = "https://pixabay.com/api/"
+// // //  const ENDPOINT = "/image_type"
+// TOKEN = "36715459-28e84205f82fac02ca5e4373e"
  
 // const options = {
 //     method: "GET",
@@ -12,13 +26,16 @@ TOKEN = "36715459-28e84205f82fac02ca5e4373e"
 //     }
 // }
 
-async function fetchImages(query, page, perPage) {
-  const response = await axios.get(
-    `?key=${TOKEN}&q=${query}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=${perPage}`,
-    )
+// axios.defaults.BASE_URL = 'https://pixabay.com/api/'
+// const TOKEN = '36715459-28e84205f82fac02ca5e4373e'
+
+// async function fetchImages(query, page, perPage) {
+//   const response = await axios.get(
+//     `?key=${TOKEN}&q=${query}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=${perPage}`,
+//      )
     
-  return response
-}
+//    return response
+//  }
 
   
    
